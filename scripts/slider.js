a6d11cf8f716fd64e8c2c90581eeee83e5a552cd
Nathan <antnefedov33@gmail.com>
2023-11-11 23:57:06 +0300

    added animation for images, background linear-gradient for app

--- scripts/slider.js
+++ scripts/slider.js
@@ -5,21 +5,44 @@ let leftArrow = document.querySelector('.leftArrow');
 let rightArrow = document.querySelector('.rightArrow');
 
 let base = './assets/images/';
-let images = ['fox', 'cat', 'parrot', 'rabbit', 'dog', 'horse', 'butterfly'];
+let images = ['fox', 'cat', 'parrot', 'rabbit', 'dog', 'swan', 'horse', 'butterfly'];
 let count = 0;
 
 leftArrow.addEventListener('click', function () {
+    resetValues();
     count -= 1;
+
     if (count < 0) {
         count = images.length - 1;
     }
-    image.setAttribute('src', `${base}${images[count]}.jpg`)
+
+    animateImage();
+    image.setAttribute('src', `${base}${images[count]}.jpg`);
+
 });
 
 rightArrow.addEventListener('click', function () {
+    resetValues();
     count += 1;
+
     if (count > images.length - 1) {
         count = 0;
     }
-    image.setAttribute('src', `${base}${images[count]}.jpg`)
+    animateImage();
+    image.setAttribute('src', `${base}${images[count]}.jpg`);
+
 })
+
+function animateImage() {
+    resetValues();
+
+    setTimeout(function () {
+        image.style.transition = 'opacity 1s';
+        image.style.opacity = 1;
+    }, 300);
+}
+
+function resetValues() {
+    image.style.opacity = 0;
+    image.style.transition = '';
+}
